b1fa66a0fb20f16ae3c675d467229940cb49ff4b
andresaldana1209 <61165319+andresaldana1209@users.noreply.github.com>
2021-06-10 16:27:44 -0500

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -18,8 +18,8 @@ if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
  * --------------------------------------------------------------------
  */
 $routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('RestCliente');
-$routes->setDefaultMethod('W_Cliente');
+$routes->setDefaultController('Home');
+$routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
 $routes->setAutoRoute(true);
@@ -32,7 +32,7 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('/', ['controller'    => 'RestCliente']);
 
 
 //---------------Controladores----------------------//
